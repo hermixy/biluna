428a946657d257814aecdd154f9a76bb6c8da1fb
Rutger <r.botermans@biluna.com>
2016-03-27 22:47:12 +0200

    Update MAC script

--- scripts/build_mac_app_new1.sh
+++ scripts/build_mac_app_new1.sh
@@ -37,23 +37,5 @@ install_name_tool -change libdb.1.dylib @executable_path/libdb.1.dylib  $appdir/
 install_name_tool -change libdb.1.dylib @executable_path/libdb.1.dylib  $appdir/Contents/MacOs/plugins/libpeng_actionplugin.dylib
 install_name_tool -change libdb.1.dylib @executable_path/libdb.1.dylib  $appdir/Contents/MacOs/plugins/libsrm_actionplugin.dylib
 
-echo "Change library paths for Assistant, does not work"
-# install_name_tool -change $qtdir/lib/QtWebKitWidgets.framework/Versions/5/QtWebKitWidgets @executable_path/../Frameworks/QtWebKitWidgets.framework/Versions/5/QtWebKitWidgets  $appdir/Contents/MacOs/Assistant
-# install_name_tool -change $qtdir/lib/QtMultimediaWidgets.framework/Versions/5/QtMultimediaWidgets @executable_path/../Frameworks/QtMultimediaWidgets.framework/Versions/5/QtMultimediaWidgets  $appdir/Contents/MacOs/Assistant
-# install_name_tool -change $qtdir/lib/QtMultimedia.framework/Versions/5/QtMultimedia @executable_path/../Frameworks/QtMultimedia.framework/Versions/5/QtMultimedia  $appdir/Contents/MacOs/Assistant
-# install_name_tool -change $qtdir/lib/QtGui.framework/Versions/5/QtGui @executable_path/../Frameworks/QtGui.framework/Versions/5/QtGui  $appdir/Contents/MacOs/Assistant
-# install_name_tool -change $qtdir/lib/QtCore.framework/Versions/5/QtCore @executable_path/../Frameworks/QtCore.framework/Versions/5/QtCore  $appdir/Contents/MacOs/Assistant
-# install_name_tool -change $qtdir/lib/QtNetwork.framework/Versions/5/QtNetwork @executable_path/../Frameworks/QtNetwork.framework/Versions/5/QtNetwork  $appdir/Contents/MacOs/Assistant
-# install_name_tool -change $qtdir/lib/QtWidgets.framework/Versions/5/QtWidgets @executable_path/../Frameworks/QtWidgets.framework/Versions/5/QtWidgets  $appdir/Contents/MacOs/Assistant
-# install_name_tool -change $qtdir/lib/QtQuick.framework/Versions/5/QtQuick @executable_path/../Frameworks/QtQuick.framework/Versions/5/QtQuick  $appdir/Contents/MacOs/Assistant
-# install_name_tool -change $qtdir/lib/QtQml.framework/Versions/5/QtQml @executable_path/../Frameworks/QtQml.framework/Versions/5/QtQml  $appdir/Contents/MacOs/Assistant
-# install_name_tool -change $qtdir/lib/QtPositioning.framework/Versions/5/QtPositioning @executable_path/../Frameworks/QtPositioning.framework/Versions/5/QtPositioning  $appdir/Contents/MacOs/Assistant
-# install_name_tool -change $qtdir/lib/QtOpenGL.framework/Versions/5/QtOpenGL @executable_path/../Frameworks/QtOpenGL.framework/Versions/5/QtOpenGL  $appdir/Contents/MacOs/Assistant
-# install_name_tool -change $qtdir/lib/QtPrintSupport.framework/Versions/5/QtPrintSupport @executable_path/../Frameworks/QtPrintSupport.framework/Versions/5/QtPrintSupport  $appdir/Contents/MacOs/Assistant
-# install_name_tool -change $qtdir/lib/QtWebKit.framework/Versions/5/QtWebKit @executable_path/../Frameworks/QtWebKit.framework/Versions/5/QtWebKit  $appdir/Contents/MacOs/Assistant
-# install_name_tool -change $qtdir/lib/QtSql.framework/Versions/5/QtSql @executable_path/../Frameworks/QtSql.framework/Versions/5/QtSql  $appdir/Contents/MacOs/Assistant
-# install_name_tool -change $qtdir/lib/QtSensors.framework/Versions/5/QtSensors @executable_path/../Frameworks/QtSensors.framework/Versions/5/QtSensors  $appdir/Contents/MacOs/Assistant
-# install_name_tool -change $qtdir/lib/QtHelp.framework/Versions/5/QtHelp @executable_path/../Frameworks/QtHelp.framework/Versions/5/QtHelp  $appdir/Contents/MacOs/Assistant
-
 echo "Creating dmg file for Biluna"
 $qtdir/bin/macdeployqt $appdir -dmg -verbose=3
